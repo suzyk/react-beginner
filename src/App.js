6ff3e6cb780c2a68f8acfca44e9833b8eaac4e77
suzyk <suzykahng@gmail.com>
2025-07-10 20:53:27 +0300

    starting movie app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,57 @@
 import Button from "./Button";
 import styles from "./App.module.css";
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
+      )
+    ).json();
+
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getMovies();
+
+    /** old way 
+    fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`)
+    .then((response) => response.json())
+    .then((json) => {
+      setMovies(json.data.movies);
+      setLoading(false);
+    })
+    */
+  }, []);
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          {movies.map((movie) => (
+            <div key={movie.id}>
+              <img src={movie.medium_cover_image} />
+              <h2>{movie.title}</h2>
+              <p>{movie.summary === "" ? "No Summary" : movie.summary}</p>
+              <ul>
+                {movie.genres.map((genre) => (
+                  <li key={genre}>{genre}</li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
+
+/*
 function Input({coinName, coinPrice = 0}) {
   const [budget, setBudget] = useState(0);
   const onChange =(event) => {
@@ -54,7 +104,7 @@ function App() {
       <Input coinName={selectedName} coinPrice={selectedPrice}/>
     </div>
   );
-}
+}*/
 
 /*
 function App() {
